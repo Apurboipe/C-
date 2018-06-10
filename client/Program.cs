a4b4850cd2500b6fcacd0b4802de0b606c6c0512
Apurboipe <apurbo.ipe@gmail.com>
2018-06-10 21:57:01 +0600

    First commit

---
+++ client/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace client
+{
+    class Program
+    {
+        private static Socket s;
+        static void Main(string[] args)
+        {
+            s=new Socket(AddressFamily.InterNetwork,SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"),1234);
+            try
+            {
+                s.Connect(ipEndPoint);
+            }
+            catch (Exception)
+            {
+                Console.Write("Unable to connect remote endpoint \r\n");
+                Main(args);
+                throw;
+            }
+            Console.Write("Enter Text");
+            string text = Console.ReadLine();
+            byte[] bytes = Encoding.ASCII.GetBytes(text);
+            s.Send(bytes);
+            Console.Write("Data send \r\n");
+            Console.Write("Press any key to continue \r\n");
+            Console.Read();
+            s.Close();
+
+
+        }
+    }
+}
---
+++ serverVersion1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace serverVersion1
+{
+    class Program
+    {
+        private static byte[] Buffer { get; set; }
+        private static Socket s;
+        static void Main(string[] args)
+        {
+            s=new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            s.Bind(new IPEndPoint(0,1234));
+            s.Listen(100);
+
+            Socket accepted = s.Accept();
+            Buffer = new byte[accepted.SendBufferSize];
+            int byteRead = accepted.Receive(Buffer);
+            byte[] formated = new byte[byteRead];
+            for (int i = 0; i < byteRead; i++)
+            {
+                formated[i] = Buffer[i];
+            }
+            string strData = Encoding.ASCII.GetString(formated);
+            Console.Write(strData + "\r\n");
+            Console.Read();
+        }
+    }
+}
